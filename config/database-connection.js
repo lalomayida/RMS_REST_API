80462e7ba8ac2cd512a3cf2c409d868e8e0dd832
lalomayida <lalomayida@gmail.com>
2020-05-13 00:51:42 -0500

    Setting prod db credentials

--- config/database-connection.js
+++ config/database-connection.js
@@ -1,8 +1,8 @@
 const db = require('pg').Pool
 module.exports = new db({
-  user: "postgres",
-  host: "localhost",
-  database: "RMS",
-  password: "postgres",
+  user: "jsegozyslmuqkr",
+  host: "ec2-3-231-16-122.compute-1.amazonaws.com",
+  database: "dbant4tpjq4q7",
+  password: "d183ce3e12a720e651d46d9716764569ed559a3d3412306f1bcb38f306c181c8",
   port: 5432
 })
